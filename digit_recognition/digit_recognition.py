e5eb71b4f46fc76990adb9cce11ddb1566125ab6
Ming <xm1223@gmail.com>
2019-03-10 13:12:59 -0700

    Update digit_recognition.py

--- digit_recognition/digit_recognition.py
+++ digit_recognition/digit_recognition.py
@@ -1,10 +1,29 @@
 # Digit Recognition
 #
 # https://www.lintcode.com/ai/digit-recognition
-#
 
 import pandas as pd
+from sklearn.linear_model import SGDClassifier
 
-
+# Load data
 train = pd.read_csv('~/ai/DigitRecognition/train.csv')
 test = pd.read_csv('~/ai/DigitRecognition/test.csv')
+# train.shape
+# test.shape
+# print(train.sample(1))
+
+# Drop the label column as train set
+X_train = train.drop('label', 1)
+# Separate lable column as train target
+y_train = train['label']
+X_test = test
+
+# Change to different model or hyperparameters
+model = SGDClassifier()
+# Train model
+model.fit(X_train, y_train)
+# Predict via model
+predictions = model.predict(X_test)
+
+submission = pd.DataFrame({'ImageId': range(1, predictions.shape[0] + 1),'Label': predictions})
+submission.to_csv('~/ai/DigitRecognition/submission.csv', index=False)
