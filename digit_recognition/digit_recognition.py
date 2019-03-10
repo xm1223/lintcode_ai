bd8d2f5e2a8aefb6bb6a4e7d7bcab20d81aaf77f
Ming <xm1223@gmail.com>
2019-03-10 12:40:27 -0700

    Create digit_recognition.py

---
+++ digit_recognition/digit_recognition.py
@@ -0,0 +1,10 @@
+# Digit Recognition
+#
+# https://www.lintcode.com/ai/digit-recognition
+#
+
+import pandas as pd
+
+
+train = pd.read_csv('~/ai/DigitRecognition/train.csv')
+test = pd.read_csv('~/ai/DigitRecognition/test.csv')
